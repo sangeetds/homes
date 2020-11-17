18e03db2fa83d1dd52b5dc67459236fc44c80690
Sangeet Narayan <65168994+sangeetds@users.noreply.github.com>
2020-11-18 01:50:30 +0530

    Update Blog.js

--- src/Blog.js
+++ src/Blog.js
@@ -7,7 +7,7 @@ export default function Project() {
         <a className = "noHover"  name = "blog"><h2 className = "heading"> Blog </h2></a> 
         <Container>
             <Row className = "blogBox">
-                <h3 style = { { marginTop: "70px" } }>So much to say <br /> yet nothign to write.</h3>
+                <h3 style = { { marginTop: "70px" } }>So much to say <br /> yet nothing to write.</h3>
                 <h3 style = { { marginTop: "40px" } }> Well this is embarassing. I've not written anything lately. Check this section out in future for any new piece</h3>
             </Row>
         </Container>
